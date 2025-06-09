086bc3f426c1aef4e084adfa303ab27cd87a1080
bekicom <adelkauz6@gmail.com>
2025-06-09 22:38:10 +0500

    git

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const session = require("express-session");
@@ -5,7 +6,6 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const mainRoutes = require("./routes/mainRoutes");
 const dbConfig = require("./config/dbConfig");
-require("dotenv").config();
 
 const app = express();
 
@@ -15,17 +15,16 @@ const corsOptions = {
   origin: [
     "https://phone-f-ten.vercel.app",
     "http://localhost:3000",
-    "http://45.136.16.207", // Backend lokal
+    "http://45.136.16.207", // 👈 VPS IP
   ],
-  methods: ["GET", "POST", "PUT", "DELETE"], // Ruxsat etilgan HTTP metodlar
+  methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
 
 app.use(cors(corsOptions));
-
 app.options("*", cors(corsOptions));
 
-app.use(bodyParser.json());
+app.use(bodyParser.json()); // 👈 SESSIONDAN OLDIN
 app.use(
   session({
     secret: process.env.JWT_SECRET,
@@ -34,7 +33,6 @@ app.use(
   })
 );
 
-// Yo'nalishlar
 app.use("/api", mainRoutes);
 
 const PORT = process.env.PORT || 8081;
